f2c6ba1f602993693084cb30e98a2b2fc0554212
vhlan <48618649+vhlan@users.noreply.github.com>
2020-12-07 12:01:04 +0800

    Update kuis.js

--- kuis.js
+++ kuis.js
@@ -1,3 +1,4 @@
-const p = document.createElement('p');
-p.textContent = 'A second line of text';
-document.body.appendChild(p);
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', () => {
+    document.body.textContent = 'Hello World!';
+  });
