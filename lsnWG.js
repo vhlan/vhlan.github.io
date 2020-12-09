e023d8f7e264d331a53f2969f1017da208e66b48
vhlan <48618649+vhlan@users.noreply.github.com>
2020-12-09 23:16:07 +0800

    lsnWG

---
+++ lsnWG.js
@@ -0,0 +1,18 @@
+var twLSN = [
+    "www.vhlan.my.id"
+];
+function cekLSN(value, arr) {
+    var status = false;
+    for (var i = 0; i < arr.length; i++) {
+        var name = arr[i];
+        if (name == value) {
+            status = true;
+            break;
+        }
+    }
+    return status;
+}
+var hst = window.location.hostname;
+if (cekLSN(hst, twLSN) == false) {
+    window.location.href = "https://www.vhlan.my.id";
+}
